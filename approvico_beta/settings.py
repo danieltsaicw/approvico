593214647898581ebf8e0e8b45273cd57af6c923
Daniel Tsai C.W <daniel.tsai.cw@gmail.com>
2019-01-26 21:38:18 -0800

    ALLOW_HOSTS

--- approvico_beta/settings.py
+++ approvico_beta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-utw0@!e9-qxr*nomri1d7wug!wdasc8yuwmen!v_qb#01q2ku'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-18-222-96-156.us-east-2.compute.amazonaws.com']
 
 
 # Application definition
